c236bdac0339b5ca38d7333cfdbf15796dea7944
Hasbiadi01 <117049361+Hasbiadi01@users.noreply.github.com>
2022-12-01 13:46:54 +0700

    Add files via upload

---
+++ OrangJava_A22100031.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author ahm
+ */
+public class Orang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String nama;
+        String alamat;
+        int umur;
+        boolean menikah;
+    }
+    String nama;
+    String alamat;
+    int umur;
+    boolean menikah;
+    
+}
